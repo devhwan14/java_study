387c29091f06f3e4c94f41bb0a087f41c3eebb01
devhwan14 <devhwan14@gmail.com>
2024-12-02 11:08:14 +0900

    [pyh] variable 최종 커밋

---
+++ lec02_variable/src/com/gn/pratice/Practice01.java
@@ -0,0 +1,12 @@
+package com.gn.pratice;
+
+public class Practice01 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 100;
+		System.out.println(a+b);
+		System.out.println(a*(b+a));
+		System.out.println(a*(b+a) / a);
+	}
+
+}
