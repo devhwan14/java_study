556a436355638c5fc395267d6ae8b189e7df9b66
devhwan14 <devhwan14@gmail.com>
2024-12-05 10:41:05 +0900

    [pyh] oop 4번째 커밋

--- lec06_oop/src/com/gn/practice05/run/Run.java
+++ lec06_oop/src/com/gn/practice05/run/Run.java
@@ -1,6 +1,7 @@
 package com.gn.practice05.run;
 
 import com.gn.practice05.model.vo.Book;
+import com.gn.study.method.constructor.model.vo.Student;
 // 1. 실행용 매소드
 // 2. 기본생성자와 매개변수 3개인 생성자,
 // 매개변수 5개인 생성자를 통해 각각의 객체 생성
@@ -24,6 +25,14 @@ public class Run {
 		
 		Book b3 = new Book("채소 과일 레시피","터치아트,","박경희",19300,0.1);
 		b3.information();
+		
+		String[] students = {"김철수","이영희","홍길동"};
+		for(int i = 0 ; i <students.length; i++) {
+			Student s = new Student();
+			s.printInfo(i);
+			s.printInfo(students[i]);
+			s.printInfo(i,students[i]);
+		}
 		
 	}
 	
